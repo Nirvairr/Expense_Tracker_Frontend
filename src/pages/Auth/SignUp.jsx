9bb67bed2b7fdd8e964b4fd53ba7faa88f1a3f66
Nikhilmukheja19 <128170642+Nikhilmukheja19@users.noreply.github.com>
2025-07-03 22:11:13 +0530

    Name Mobile

--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -76,7 +76,7 @@ const SignUp = () => {
   };
   return (
     <AuthLayout>
-      <div className="w-full max-w-[500px] h-full mt-10 md:mt-0 px-4 mx-auto flex flex-col justify-center">
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
         <h3 className="text-xl font-semibold text-black">Create an Account</h3>
         <p className="text-xs text-slate-700 mt-[5px] mb-6">
           Join us today by entering your details below.
